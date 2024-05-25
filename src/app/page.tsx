6e6917bf21dcfa276f1c51f0d5c1ea11e449d472
Vinh Vu <vinhhv@uci.edu>
2024-05-25 16:31:08 -0700

    Add login auth functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 import { Button } from "@nextui-org/react";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click me!!!</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed out</div>
+      )}
+      <Profile />
     </div>
   );
 }
