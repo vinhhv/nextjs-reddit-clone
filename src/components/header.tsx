561b679c403a6bf0fa4f29796a10df56fecc45d0
Vinh Vu <vinhhv@uci.edu>
2024-05-25 17:42:50 -0700

    Connect server actions to login frontend

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import * as actions from "@/actions";
 import { auth } from "@/auth";
 import {
   Avatar,
@@ -7,6 +8,9 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@nextui-org/react";
 import Link from "next/link";
 
@@ -15,20 +19,37 @@ export default async function Header() {
 
   let authContent: React.ReactNode;
   if (session?.user) {
-    authContent = <Avatar src={session.user.image || ""} />;
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger className="hover:cursor-pointer">
+          <Avatar src={session.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
   } else {
     authContent = (
       <>
         <NavbarItem>
-          <Button type="submit" color="secondary" variant="bordered">
-            Sign In
-          </Button>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
         </NavbarItem>
 
         <NavbarItem>
-          <Button type="submit" color="primary" variant="flat">
-            Sign Up
-          </Button>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
         </NavbarItem>
       </>
     );
