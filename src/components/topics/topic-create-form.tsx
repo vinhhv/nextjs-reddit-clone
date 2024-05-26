76308d76ff423c34bc7d49e821321678a02c4768
Vinh Vu <vinhhv@uci.edu>
2024-05-25 19:55:23 -0700

    Add zod validation for topic creation

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { createTopic } from "@/actions";
 import {
   Button,
@@ -7,19 +9,30 @@ import {
   PopoverTrigger,
   Textarea,
 } from "@nextui-org/react";
+import { useFormState } from "react-dom";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(createTopic, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+            />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
