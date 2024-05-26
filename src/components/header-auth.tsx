1a0f8bc37b6bed3e368581edfcdb56bf2f67623b
Vinh Vu <vinhhv@uci.edu>
2024-05-25 19:24:29 -0700

    Move auth to client side to maintain static homepage

---
+++ src/components/header-auth.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import * as actions from "@/actions";
+import {
+  Avatar,
+  Button,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+
+export default function HeaderAuth() {
+  const session = useSession();
+
+  let authContent: React.ReactNode;
+  if (session.status === "loading") {
+    authContent = null;
+  } else if (session.data?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger className="hover:cursor-pointer">
+          <Avatar src={session.data.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return authContent;
+}
