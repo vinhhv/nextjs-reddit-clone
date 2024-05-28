3e93d400e4927645e4c9523a3fdaf7c4ac34dc5c
Vinh Vu <vinhhv@uci.edu>
2024-05-28 12:53:31 -0700

    Post show page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,27 @@
-export default function PostShowPage() {
-  return <div>Post Show Page</div>;
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import CommentList from "@/components/comments/comment-list";
+import PostShow from "@/components/posts/post-show";
+import paths from "@/paths";
+import Link from "next/link";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      {/* <CommentList comments={comments} /> */}
+    </div>
+  );
 }
