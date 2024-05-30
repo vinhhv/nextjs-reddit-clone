02dd71e8ecbea4933a1e0da1cd9f2df6198a1696
Vinh Vu <vinhhv@uci.edu>
2024-05-30 16:16:56 -0700

    Render top posts

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -11,7 +11,6 @@ export type CommentWithAuthor = Comment & {
 
 export const fetchCommentsByPostId = cache(
   (postId: string): Promise<CommentWithAuthor[]> => {
-    console.log("Making a query");
     return db.comment.findMany({
       where: { postId },
       include: {
