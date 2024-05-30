19c2ba25a80553c91983c39022f4a8db683354f3
Vinh Vu <vinhhv@uci.edu>
2024-05-30 16:43:09 -0700

    Fix deopted warning

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,7 @@ import {
   NavbarItem,
 } from "@nextui-org/react";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Header() {
   return (
@@ -18,7 +19,9 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify="center">
         <NavbarItem>
-          <SearchInput />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
